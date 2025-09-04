108eaa4118fde7b27e13e0c36bc09d31e7296d0e
shornaa6 <naharshorna16@gmail.com>
2025-09-04 22:11:37 +0600

    companies-lines

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html' , './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'primary': '#3238F2'
+        primary: "#3238F2", // custom primary color
       },
       fontFamily: {
-        'display': ['poppins', 'sans-serif'],
-        'body' : ['inter' , 'sans-serif']
-      }
+        display: ["Poppins", "sans-serif"], // headings
+        body: ["Inter", "sans-serif"],      // body text
+      },
+      scale: {
+        "-1": "-1", // flip support (scale-x-[-1])
+      },
+      translate: {
+        // custom translate values
+        "-36": "-9rem",   // like -translate-x-36
+        "-48": "-12rem",  // like -translate-x-48
+      },
     },
   },
   plugins: [],
-}
+};
